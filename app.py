3f6460953c7d2391a26512387ffd3b8a03dd5caf
Gus <gustavorincon37@gmail.com>
2019-05-20 00:30:22 -0400

    - Improved the structure of the project with jinja templates. - Improved css.

---
+++ app.py
@@ -0,0 +1,26 @@
+import csv
+import json
+from flask import Flask, jsonify, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Reload templates when they are changed
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+projects = open('templates/projects.json')
+
+projects = json.loads(projects.read())
+
+@app.after_request
+def after_request(response):
+    """Disable caching"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def get_index():
+    return render_template("main.html", projects=projects["projects"])
